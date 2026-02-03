ee11da76afb74c1a795e314c774c701d15f8b921
Tekina <nm0321@yandex.ru>
2026-02-03 17:16:52 +0300

    debug

--- 02-routes/files/getFileList.ts
+++ 02-routes/files/getFileList.ts
@@ -1,5 +1,6 @@
 
 export default async function (ctx)  {
 //[ "request", "store", "qi", "path", "set", "headers", "cookie", "query", "body" ]
+    console.log(ctx.store)
     return ctx.store.chokidar
 }
